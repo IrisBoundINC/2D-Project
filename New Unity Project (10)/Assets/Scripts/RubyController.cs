ba3e7451854ae598cd02c2731e3adb4a826d8596
IrisBoundINC <LevitatedEnergy@gmail.com>
2021-10-19 18:34:22 -0500

    Initial commit

--- New Unity Project (10)/Assets/Scripts/RubyController.cs
+++ New Unity Project (10)/Assets/Scripts/RubyController.cs
@@ -4,41 +4,17 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    public int = maxHealth = 5;
-    private int = currentHealth;
-       
-        
-    private void Start()
-    {
-        currentHealth = maxHealth;
-    }
-
-
-
-
+    // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log(vertical);
-
         Vector2 position = transform.position;
-        position.x = position.x + 0.3f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.3f * vertical * Time.deltaTime;
+        position.x = position.x + 0.1f;
         transform.position = position;
-    }
-
-    void ChangeHealth(int amount)
-    {
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log("Current Health: " + currentHealth + "/" maxHealth);
     }
 }
